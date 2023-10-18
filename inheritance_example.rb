fef4f39c15fc05f08de43e7a1a0ec624d53bde30
bshin27 <125755440+bshin27@users.noreply.github.com>
2023-10-18 12:28:57 -0500

    added comments

--- inheritance_example.rb
+++ inheritance_example.rb
@@ -1,3 +1,4 @@
+# made vehicle class that has all similarities of both classes that will be inheriting all of the info in vehicle (Vehicle is parent class)
 class Vehicle
   def initialize
     @speed = 0
@@ -17,12 +18,14 @@ class Vehicle
   end
 end
 
+# Car class inherits all info in Vehicle class and has new method added onto it (Car is daughter class)
 class Car < Vehicle
   def honk_horn
     puts "Beeeeeeep!"
   end
 end
 
+# Bike class inherits all info in Vehicle class and has new mehtod added onto it (Bike is daughter class)
 class Bike < Vehicle
   def ring_bell
     puts "Ring ring!"
