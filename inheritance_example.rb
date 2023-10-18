1097ba865cc7762f758af63499c70e364d319013
bshin27 <125755440+bshin27@users.noreply.github.com>
2023-10-18 12:51:57 -0500

    added initialize to each class and put super

--- inheritance_example.rb
+++ inheritance_example.rb
@@ -20,6 +20,17 @@ end
 
 # Car class inherits all info in Vehicle class and has new method added onto it (Car is daughter class)
 class Car < Vehicle
+  def initialize
+    super                     # super adds all info from a method that has the same name and allows additions to be added after
+    @fuel = "Full"
+    @make = "Nissan"
+    @model = "Altima"
+  end
+
+  def fuel_level
+    pp @fuel
+  end
+
   def honk_horn
     puts "Beeeeeeep!"
   end
@@ -27,6 +38,16 @@ end
 
 # Bike class inherits all info in Vehicle class and has new mehtod added onto it (Bike is daughter class)
 class Bike < Vehicle
+  def initialize
+    super
+    @type = "Road bicycle"
+    @weight = 30
+  end
+
+  def type
+    pp @type
+  end
+
   def ring_bell
     puts "Ring ring!"
   end
@@ -37,6 +58,8 @@ bike = Bike.new
 
 pp car.accelerate
 car.honk_horn
+car.fuel_level
 
 pp bike.accelerate
 bike.ring_bell
+bike.type
